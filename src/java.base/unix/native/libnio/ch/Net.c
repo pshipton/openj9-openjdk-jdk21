9bdff3fc12cac6c12fd5899c89bda68178158a82
Peter Shipton <Peter_Shipton@ca.ibm.com>
2025-01-30 15:58:36 -0500

    2

--- src/java.base/unix/native/libnio/ch/Net.c
+++ src/java.base/unix/native/libnio/ch/Net.c
@@ -385,7 +385,7 @@ Java_sun_nio_ch_Net_connect0(JNIEnv *env, jclass clazz, jboolean preferIPv6,
         Trc_nio_ch_Net_connect4((jlong)fd, inet_ntop(AF_INET, &sa.sa4.sin_addr, buf, sizeof(buf)), port, sa_len);
     } else if (AF_INET6 == sa.sa6.sin6_family) {
         char buf[INET6_ADDRSTRLEN];
-        Trc_nio_ch_Net_connect6((jlong)fd, NULL /*inet_ntop(AF_INET6, &sa.sa6.sin6_addr, buf, sizeof(buf))*/, port, ntohl(sa.sa6.sin6_scope_id), sa_len);
+        Trc_nio_ch_Net_connect6((jlong)fd, inet_ntop(AF_INET6, &sa.sa6.sin6_addr, buf, sizeof(buf)), port, ntohl(sa.sa6.sin6_scope_id), sa_len);
     }
 
     rv = connect(fd, &sa.sa, sa_len);
